edf53e2aa1df8d21eddd7034a772e7d1462ad409
borgearthur <tubodish@gmail.com>
2024-09-18 11:21:06 -0300

    add model simplificado de Produto

--- projeto_fds/mercado/admin.py
+++ projeto_fds/mercado/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
+# Função para registrar um modelo se não estiver registrado
+def register_model(admin_site, model):
+    if model not in admin_site._registry:
+        admin_site.register(model)
+    else:
+        print(f"O modelo {model.__name__} já está registrado.")
+        
 # Register your models here.
+register_model(admin.site, Produto)
+register_model(admin.site, Foto)
--- projeto_fds/mercado/models.py
+++ projeto_fds/mercado/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome_produto = models.CharField(max_length=50, null=True)
+    descricao = models.TextField(blank=False, default='')
+
+    def __str__(self):
+        return self.nome_produto
+
+class Foto(models.Model):
+    produto = models.ForeignKey(Produto, related_name='fotos', on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to='fotos_produto/', blank=True, null=True)
+
+    def __str__(self):
+        return f"Foto para {self.produto.nome_produto}"
