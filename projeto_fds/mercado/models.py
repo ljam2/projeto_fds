748d73690707341682ee221090e5ea7ce9e72f7e
borgearthur <tubodish@gmail.com>
2024-09-23 10:42:30 -0300

    adicionando usuario cliente

--- projeto_fds/mercado/models.py
+++ projeto_fds/mercado/models.py
@@ -1,5 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models import Avg
+from datetime import datetime
 
+class UserCliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    nome_completo = models.CharField(max_length=150, default="Desconhecido")
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255, null=True)
+    confirm_password = models.CharField(max_length=255, null=True)
+    is_supplier = models.BooleanField(default=False)
+    profile_image = models.ImageField(upload_to='profile_image/', blank=True, null=True)
+
+    def __str__(self):
+        return self.email
+    
 class Produto(models.Model):
     nome_produto = models.CharField(max_length=50, null=True)
     descricao = models.TextField(blank=False, default='')
