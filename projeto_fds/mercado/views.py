9bc09d6187d04ee7f7fec46071a2cba589a33836
borgearthur <tubodish@gmail.com>
2024-09-23 10:26:39 -0300

    view não funcional

--- projeto_fds/mercado/views.py
+++ projeto_fds/mercado/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import *
+from django.views import View
+from django.http import HttpResponse, Http404
 
 # Create your views here.
+
+class ViewFoto(View):
+    def get(self, request, foto_id):
+        try:
+            foto = Foto.objects.get(pk=foto_id)
+        except Foto.DoesNotExist:
+            raise Http404("Foto não existe")
+        context = {'Foto' : foto}
+        return render(request, 'detail.html', context)
